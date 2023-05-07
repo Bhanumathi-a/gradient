473f4aa00980b037b9562718161470aba7c65743
Bhanumathi-a <61862519+Bhanumathi-a@users.noreply.github.com>
2023-05-07 09:16:39 +0530

    Add files via upload
    updated v2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-let clickFun = function () {
+//nav
+let clickFunNav = () => {
   if (!event.target.classList.contains("nav-link")) return
   event.target.classList.add("active")
   let links = document.querySelectorAll(".nav-link")
@@ -7,4 +8,26 @@ let clickFun = function () {
     links[i].classList.remove("active")
   }
 }
-document.addEventListener("click", clickFun, false)
+document.addEventListener("click", clickFunNav, false)
+
+//info dropdown homepage
+let infoHome = document.querySelector(".info .title")
+
+let clickFunInfo = () => {
+  infoHome.classList.toggle("active")
+}
+
+document.addEventListener("click", clickFunInfo)
+// console.log(infoHome)
+
+//searchbox
+let searchBar = document.getElementById("searchBar")
+let clickFunSearch = () => {
+  searchBar.classList.add("toggleClass")
+}
+searchBar.addEventListener("click", clickFunSearch)
+window.addEventListener("mouseup", (e) => {
+  if (e.target != searchBar && e.target.parentNode != searchBar) {
+    searchBar.classList.remove("toggleClass")
+  }
+})
